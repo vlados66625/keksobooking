a80353ec99802400d29f59fe2bc2bd34b7e60434
Владислав <vlados66625@mail.ru>
2024-09-27 18:41:09 +0300

    7 дз готовое

--- js/generating-popup.js
+++ js/generating-popup.js
@@ -1,8 +1,4 @@
-const popup = document.querySelector('#card')
-  .content
-  .querySelector('.popup');
-
-const typeAccoommodation = {
+const typeAccommodation = {
   flat: 'Квартира',
   bungalow: 'Бунгало',
   house: 'Дом',
@@ -10,45 +6,83 @@ const typeAccoommodation = {
   hotel: 'Отель',
 };
 
+const addsDataTextContent = (element, text) => {
+  if (element) {
+    if (text || text === 0) {
+      element.textContent = text;
+      return element;
+    }
+    element.remove();
+  }
+};
+
+const addsDataSrc = (element, src) => {
+  if (src) {
+    if (src !== null && src !== undefined) {
+      element.src = src;
+      return element;
+    }
+    element.remove();
+  }
+};
+
+const popup = document.querySelector('#card')
+  .content
+  .querySelector('.popup');
+
 const generatingPopup = (data) => {
-  const fragment = document.createDocumentFragment();
+  if (popup) {
+    const fragment = document.createDocumentFragment();
 
-  for (const itemData of data) {
-    const popupClone = popup.cloneNode(true);
-    popupClone.querySelector('.popup__avatar').src = itemData.author.avatar;
-    popupClone.querySelector('.popup__title').textContent = itemData.offer.title;
-    popupClone.querySelector('.popup__text--address').textContent = itemData.offer.address;
-    popupClone.querySelector('.popup__text--price').textContent = `${itemData.offer.price} ₽/ночь`;
-    popupClone.querySelector('.popup__type').textContent = typeAccoommodation[itemData.offer.type];
-    popupClone.querySelector('.popup__text--capacity').textContent = `${itemData.offer.rooms} комнаты для ${itemData.offer.guests} гостей`;
-    popupClone.querySelector('.popup__text--time').textContent = `Заезд после ${itemData.offer.checkin}, выезд до ${itemData.offer.checkout}`;
+    for (const itemData of data) {
+      const popupClone = popup.cloneNode(true);
+      addsDataSrc(popupClone.querySelector('.popup__avatar'), itemData?.author?.avatar);
+      addsDataTextContent(popupClone.querySelector('.popup__title'), itemData?.offer?.title);
+      addsDataTextContent(popupClone.querySelector('.popup__text--address'), itemData?.offer?.address);
+      const textPrice = itemData?.offer?.price ? `${itemData.offer.price} ₽/ночь` : null;
+      addsDataTextContent(popupClone.querySelector('.popup__text--price'), textPrice);
+      const textType = itemData?.offer?.type ? typeAccommodation[itemData.offer.type] : null;
+      addsDataTextContent(popupClone.querySelector('.popup__type'), textType);
+      const textCapacity = itemData?.offer?.rooms && itemData?.offer?.guests ? `${itemData.offer.rooms} комнаты для ${itemData.offer.guests} гостей` : null;
+      addsDataTextContent(popupClone.querySelector('.popup__text--capacity'), textCapacity);
+      const textTime = itemData?.offer?.checkin && itemData?.offer?.checkout ? `Заезд после ${itemData.offer.checkin}, выезд до ${itemData.offer.checkout}` : null;
+      addsDataTextContent(popupClone.querySelector('.popup__text--time'), textTime);
 
-    const popupFeatures = popupClone.querySelectorAll('.popup__feature');
-    const features = itemData.offer.features;
+      const popupFeatures = popupClone.querySelectorAll('.popup__feature');
+      const features = itemData?.offer?.features || [];
 
-    for (const popupFeature of popupFeatures) {
-      const isPresent = features.some((feature) => popupFeature.classList.contains(`popup__feature--${feature}`));
-      if (!isPresent) {
-        popupFeature.remove();
+      for (const popupFeature of popupFeatures) {
+        const isPresent = features.some((feature) => popupFeature.classList.contains(`popup__feature--${feature}`));
+        if (!isPresent) {
+          popupFeature.remove();
+        }
       }
+
+      addsDataTextContent(popupClone.querySelector('.popup__description'), itemData?.offer?.description);
+
+      const popupPhotosContainer = popupClone.querySelector('.popup__photos');
+      const photosSrc = itemData?.offer?.photos || [];
+      let popupPhoto = popupClone.querySelector('.popup__photo');
+      if (popupPhoto) {
+        if (photosSrc.length > 0) {
+          photosSrc.forEach((photo, index) => {
+            if (index >= 1) {
+              popupPhoto = popupPhoto.cloneNode(true);
+              popupPhoto.src = photo;
+              popupPhotosContainer.append(popupPhoto);
+            } else {
+              popupPhoto.src = photo;
+            }
+          });
+        } else {
+          popupPhotosContainer.remove();
+        }
+      }
+      fragment.append(popupClone);
     }
 
-    popupClone.querySelector('.popup__description').textContent = itemData.offer.description;
-    let popupPhoto = popupClone.querySelector('.popup__photo');
-    itemData.offer.photos.forEach((photo, index) => {
-      if (index >= 1) {
-        popupPhoto = popupPhoto.cloneNode(true);
-        popupPhoto.src = photo;
-        popupClone.querySelector('.popup__photos').append(popupPhoto);
-      } else {
-        popupPhoto.src = photo;
-      }
-    });
-
-    fragment.append(popupClone);
+    return (fragment);
   }
-
-  return (fragment);
 };
 
 export { generatingPopup };
